e2dc6e1898d4ea1e337d0d8c33090716c36595d2
Jan Hartmann <kontakt@janhartmann.dk>
2015-01-27 17:41:21 +0100

    Changes to packages

--- NerveFramework.UnitTests/Properties/AssemblyInfo.cs
+++ NerveFramework.UnitTests/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NerveFramework.UnitTests")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Unit Test library for the Nerve Framework")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Jan Hartmann")]
 [assembly: AssemblyProduct("NerveFramework.UnitTests")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
